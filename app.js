0b7e40073f27e414494ec4c11252bb8493a41ad7
mari <mariroman604@gmail.com>
2023-02-05 15:48:44 +0100

    Sliders

---
+++ app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+
+const slides = document.querySelectorAll('.slide');
+
+slides.forEach((slide) => {
+    slide.addEventListener('click', () => {
+
+        clearActiveClasses();
+        slide.classList.add('active');
+    
+    });
+
+});
+
+function clearActiveClasses() {
+    slides.forEach((slide) => {
+        slide.classList.remove('active');
+
+    });
+
+}
+
